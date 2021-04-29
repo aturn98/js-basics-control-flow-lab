6ee0cfd6592a53f329947fd1f6d59a0c06fad6b5
Austin <aturn9880@gmail.com>
2021-04-29 15:17:30 -0400

    commit tree 7ed3961026dbadd94ff1c1ef919fab1e0819f77b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someValue){
+  const response = (someValue === "NYC") ? "Ok, sounds good." : "No go."
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someValue){
+  switch (someValue) {
+    case "generous" : 
+      return "Thank you so much.";
+      break;
+    case "not as generous" :
+      return "Thank you."
+      break;
+    case "thanks for everything" :
+      return "Bye."
+      break;
+  }
 }
